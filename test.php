214e9b535281398d2b1167c65c27c911c3e951e9
amber-libby <amber.libby7@gmail.com>
2017-01-05 09:19:17 -0500

    First Test Commit
    A test file and a test commit

---
+++ test.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * User: Amber Libby
+ * Date: 1/5/2017
+ * Time: 9:17 AM
+ */
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body>
+		<p> Test Pull and commit </p>
+	</body>
+</html>
