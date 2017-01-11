5a81026ccbca1ebf35994779d58b61e13331e9ff
amber-libby <amber.libby7@gmail.com>
2017-01-11 17:06:08 -0500

    Added features and new layout

--- display.php
+++ display.php
@@ -10,7 +10,7 @@
 <html>
     <head>
         <link rel="stylesheet" type="text/css" href="./css/display.css" />
-        <title></title>
+        <title>Lab-Display</title>
         <script src="https://code.jquery.com/jquery-latest.js"></script>
         <script src="./scripts/display.js"></script>
 		
@@ -22,7 +22,7 @@
 		<script type='text/javascript'>
 			$(document).ready(function() 
 			{
-				var requestUrl = 'http://api.rit.edu/v1/rooms/070-2320/meetings?RITAuthorization=95da72f6e649aff4f45405cded98a109cb1514da';
+				var requestUrl = 'http://api.rit.edu/v1/rooms/'+'070-2620'+'/meetings?RITAuthorization=95da72f6e649aff4f45405cded98a109cb1514da';
 				var request = new XMLHttpRequest();
 				request.open('GET', requestUrl);
 				request.responseType = 'json';
@@ -40,21 +40,20 @@
 					//test.textContent = JSON.stringify(data);
 					//body.appendChild(test);
 					
-					//Working on getting the values from the data into the correct JSON format needed for fullCalendar
+					//Contains all the data from the request URL in 
 					var dataObj = JSON.parse(JSON.stringify(data));
 					
-					//Today's Date
+					//Today's Date and Time
 					//var todayDate = moment().format('YYYY-MM-DD');
-					var todayDate = '2016-10-28';
+					var todayDate = '2016-11-04';
+					
+					/* NOT THE CORRECT MINUTES */
 					var todayTime = moment().format('HH:MM:SS');
 					
 					
-					//JSON object creation for the today's current events
-					var todaysEventsObj = '';
 					
 					
-					
-					//Display calendar -- pass in objects
+					//Display the calendar and define defaults
     				$('#calendar').fullCalendar({
 						defaultView: 'agendaDay',
 						header:{
@@ -62,43 +61,73 @@
 							center: false,
 							right: false
 						},
-						defaultDate: '2016-10-28',
+						defaultDate: '2016-11-04',
 						minTime: '08:00:00',
 						maxTime: '22:00:00',
 						contentHeight: 'auto',
-						allDaySlot: false,
-						slotDuration: '00:60:00'
+						allDaySlot: false
 					
 					});
 					
-					
+					//array to determine the lab status
+					var todaysEventTimesArr = [];
+							
 					//Date and time of all events
 					for(var i=0, len = dataObj.data.length; i < len; i++){
-						//document.getElementById('testingID').innerHTML += dataObj.data[i].date + ' ' + dataObj.data[i].start + '</br>';
+						//The Date that the event will begin
 						var eventStartDate = dataObj.data[i].date;
 						
-						//is the event on the currentDate
+						//check if the event is on the currentDate -- only render events that will show to save time
 						if (todayDate == eventStartDate){
-							//document.getElementById('testingID').innerHTML += dataObj.data[i].date + ' ' + dataObj.data[i].start + '</br>';
-							
+							//Create a proper string of when the events are beginning and ending
 							var eventStart = dataObj.data[i].date + ' ' + dataObj.data[i].start;
 							var eventEnd = dataObj.data[i].date + ' ' + dataObj.data[i].end;
 							
 							//creation of the events object in the format fullcalendar requires
-							todaysEventsObj = todaysEventsObj.concat("{title: '"+ dataObj.data[i].meeting + "',start:'" + eventStart + "', end: '" + eventEnd + "'},");
 							var eventObj = '{"title": "'+ dataObj.data[i].meeting + '","start":"' + eventStart + '", "end": "' + eventEnd + '"}';
 							var finalEventObj = JSON.parse(eventObj);
-							$('#calendar').fullCalendar( 'renderEvent', finalEventObj);														
+							//Add events to the fullcalendar 
+							$('#calendar').fullCalendar( 'renderEvent', finalEventObj);	
+							
+							
+							//working on determining the lab status by adding start and end times to an array
+							var eventStartTime = dataObj.data[i].start;
+							var eventEndTime = dataObj.data[i].end;
+							//even indexes are start -- odd indexes are end
+							todaysEventTimesArr.push(eventStartTime, eventEndTime);
+							
+						}
+					
+							
+					}//end of for
+					
+					//go through  events that were on today -- todaysEventTimesArr
+					//Determine Labstatus ---- *****NOT YET WORKING****
+					var labStatus = 'No Status';
+					for(var i=0, len = todaysEventTimesArr.length; i < len; i++){	
+						//then lab is closed
+						
+						if(todayTime>=todaysEventTimesArr[i] && todayTime<=todaysEventTimesArr[i+1]){
+							labStatus = 'Closed';
+							console.log('Closed '+ todayTime);
+							return labStatus;
+						}
+						//then lab is open
+						else{
+							labStatus = 'Open';
+							console.log('Open '+ todayTime);
+							return labStatus;
 						}
 					}
-					todaysEventsJSON = todaysEventsObj.slice(0,-1);					
-					console.log(eventObj);					
-				}					
-			});
+					console.log(todaysEventTimesArr);
+				}//end of request.onload()					
+			});//end of .ready()
+			
 
 		</script>
     </head>
-    <body>
+  
+  <body>
         <div class="container">
             <div id="top" class="top-container">
                 <div id="top" class="top-title">
@@ -108,58 +137,105 @@
                     <span class="top-status-text">Closed</span>
                 </div>
             </div>
-            <div id="middle" class="middle-container">
-                <div id="middle" class="middle-labassistants">
 				
 				<!--Calendar Here-->
 				<div id='calendar'></div>
-				<?php
-				//reach api and copy contents into a var
-				//$url = "http://api.rit.edu/v1/rooms/070-2160/meetings?RITAuthorization=95da72f6e649aff4f45405cded98a109cb1514da";
-				//$json = file_get_contents($url);
-				//$json_data = json_decode($json, true);
-				//var_dump($json_data);
-				//echo "My token: ". $json_data["access_token"];
-				
-				//current date
-				//$currentDate = date("Y-m-d");
-				//$currentDate = '2016-12-06';
-				
-				//get data from the array
-				//foreach ($json_data[data] as $event){
-					
-					//only display if events are on the current date
-					//if($event['date'] === $currentDate){
-							//echo 'Date: '.$event['date'];
-							//echo ', Start Time: '.$event['start'];
-							//echo ', End Time: '.$event['end'];
-							//echo ', Meeting: '.$event['meeting'];
-							//echo ', Meeting Type: '.$event['meetingtype'];
-							//echo ', Room Number: '.$event['room']['room'].'<br/>';
-					//}
-				//}
-				
-				
-				?>
-	
-			
-				
-                </div>
-                <div id="middle" class="middle-teachingassistants">
-
-                </div>
-            </div>
-            <div id="bottom" class="bottom-container">
-                 <!-- Labbies and TA's 
-                <div id="bottom" class="bottom-staff">
-                   
-					
-                </div> -->
-				<div id="bottom" class="bottom-clock">
+		
+               <div id='labStaff'>
+			   
+					<?php
+						define('SCHEDULE_NAME_LAB_ASSISTANT', 'Lab Assistant');
+						define('SCHEDULE_NAME_TEACHING_ASSISTANT', 'Teaching Assistant');
+						include("modules/simple_html_dom.php");
+						$html = file_get_html("http://work.cias.rit.edu/ist/widgets/unifiedschedule");
+						/*
+						 * <table><tr> in the widgetContent
+						 * The result should be a <td> which contains tables
+						 * Each table is a schedule
+						 */
+						$widgetCells = $html->find("table#widgetContent tr td");
+						
+						foreach ($widgetCells as $widgetCell) {	
+							
+							/* Retrieve Lab Assistant Schedule */
+							/* The title should be in the <th> */
+							$title = $widgetCell->find('th', 0)->plaintext;
+							/* Test to see if this is the lab assistant table */
+							if (stripos($title, SCHEDULE_NAME_LAB_ASSISTANT, 0) !== false) {
+								
+								/*
+								 * A span that contains the lab assistant's name should look like this
+								 * <div  class='list-group-item' style='background-color: #B39EB5'>
+								 * <strong>
+								 * <span id='replaceme' />Kyle Bansavage
+								 * </strong>
+								 * </div>
+								 * </div>
+								 */
+								$labAssistants = $widgetCell->find('div.list-group-item strong');
+								
+								foreach ($labAssistants as $labAssistant) {
+									$labAssistantName = $labAssistant->plaintext;
+									//echo $labAssistantName;
+									$labAssistantResults[] = $labAssistantName;
+								}
+								
+							}
+								
+							/* Retrieve Teaching Assistant Schedule */
+							/* The title should be in the <th> */	
+							$title = $widgetCell->find('th', 0)->plaintext;
+							/* Test to see if this is the teaching assistant table */
+							if (stripos($title, SCHEDULE_NAME_TEACHING_ASSISTANT, 0) !== false) {
+								/* TA Names */
+								$TAs = $widgetCell->find('div.list-group-item strong');
+								foreach ($TAs as $TA) {
+									$TAName = $TA->plaintext;
+									//echo $TAName;
+									$teachingAssistantResults[] = $TAName;
+								}
+							}
+							
+							
+						}
+						
+					?>
+					<table id="labAssistantTable">
+						<tr>
+							<th>Lab Assistants</th>
+						</tr>
+						<?php
+							if(!empty($labAssistantResults)){
+								foreach($labAssistantResults as $v){
+									echo '<tr><td>'.$v.'</td></tr>';
+								}
+							}
+							else{
+								echo '<tr><td>None on Duty</td></tr>';
+							}
+						?>
+					</table>
+					<table id="teachingAssistantTable">
+						<tr>
+							<th>Teaching Assistants</th>
+						</tr>
+						<?php
+							if(!empty($teachingAssistantResults)){
+								foreach($teachingAssistantResults as $v){
+									echo '<tr><td>'.$v.'</td></tr>';
+								}
+							}
+							else{
+								echo '<tr><td>None on Duty</td></tr>';
+							}
+						?>
+					</table>
+				</div>
+            
+				<div class="bottom-clock">
                     <p id="clock"></p>
                     <p id="fulldate"></p>
                 </div>
-            </div>
         </div>
     </body>
 </html>
--- widget.php
+++ widget.php
@@ -7,6 +7,7 @@
  */
 
 define('SCHEDULE_NAME_LAB_ASSISTANT', 'Lab Assistant');
+define('SCHEDULE_NAME_TEACHING_ASSISTANT', 'Teaching Assistant');
 
 include("modules/simple_html_dom.php");
 
@@ -21,9 +22,11 @@ $widgetCells = $html->find("table#widgetContent tr td");
 
 
 foreach ($widgetCells as $widgetCell) {
-    /* The title should be in the <th> */
+	
+	/* Retrieve Lab Assistant Schedule */
+    
+	/* The title should be in the <th> */
     $title = $widgetCell->find('th', 0)->plaintext;
-
     /* Test to see if this is the lab assistant table */
     if (stripos($title, SCHEDULE_NAME_LAB_ASSISTANT, 0) !== false) {
         $results[0] = "Lab Assistants";
@@ -35,8 +38,8 @@ foreach ($widgetCells as $widgetCell) {
          * 04:00 pm - 07:00 pm
          * </div>
          */
-        $time = $widgetCell->find('.list-group-item-info', 0)->plaintext;
-        echo $time;
+        $LAtime = $widgetCell->find('.list-group-item-info', 0)->plaintext;
+        echo $LAtime;
 
         /*
          * A span that contains the lab assistant's name should look like this
@@ -51,5 +54,27 @@ foreach ($widgetCells as $widgetCell) {
             $labAssistantName = $labAssistant->plaintext;
             echo $labAssistantName;
         }
+		
+    }
+	
+	
+	/* Retrieve Teaching Assistant Schedule */
+	/* The title should be in the <th> */	
+	$title = $widgetCell->find('th', 0)->plaintext;
+	/* Test to see if this is the teaching assistant table */
+	if (stripos($title, SCHEDULE_NAME_TEACHING_ASSISTANT, 0) !== false) {
+        $results[0] = "Teaching Assistants";
+		
+		/* Time scheduled for the TA's*/
+		$TAtime = $widgetCell->find('.list-group-item-info', 1)->plaintext;
+        echo $TAtime;
+		
+		/* TA Names */
+		$TAs = $widgetCell->find('.list-group-item');
+        foreach ($TAs as $TA) {
+            $TAName = $TA->plaintext;
+            echo $TAName;
+		}
     }
 }
+?>
