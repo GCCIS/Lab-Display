037a305fc02d1805f92d87726bf53cdd6941b995
amber-libby <amber.libby7@gmail.com>
2017-08-08 13:17:15 -0400

    remove setTimeout

--- display.php
+++ display.php
@@ -240,127 +240,124 @@
 			* @param {string} todayDate - the date today
 			*/
 			function createMiniSchedule(roomName, roomNumber, closeTime, openTime, todayDate){
-				//set an interval so this part of the page is updated and always accurate
-				setTimeout(function(){
-					//get all of the events that are happening in this room today
-					$.getJSON('http://api.rit.edu/v1/rooms/'+roomNumber+'/meetings?date='+todayDate+'&RITAuthorization=95da72f6e649aff4f45405cded98a109cb1514da',
-						function (json){
-							//variables used to check to see if the lab is closed
-							var clock = new Date();
-							var h = addZero(clock.getHours());
-							var m = addZero(clock.getMinutes());
-							var currentTimeNoSec = h + ":" + m;
-							
-							//store the strings based on whether the id already exists on the page or not
-							var str = "";
-							
-							//if the lab is closed then you don't need to check anything else - just print the open time
-							if(currentTimeNoSec <= openTime || currentTimeNoSec >= closeTime){
-								//the lab is closed so you should show the closed response
-								str = "<table id="+roomNumber+" style='background-color: #9e0b0f;' width='100%'><tr><td><b>"+roomName + "</b></td> <td>Lab opens at "+convertTimeTo12(openTime)+"</td></tr></table>";
+				//get all of the events that are happening in this room today
+				$.getJSON('http://api.rit.edu/v1/rooms/'+roomNumber+'/meetings?date='+todayDate+'&RITAuthorization=95da72f6e649aff4f45405cded98a109cb1514da',
+					function (json){
+						//variables used to check to see if the lab is closed
+						var clock = new Date();
+						var h = addZero(clock.getHours());
+						var m = addZero(clock.getMinutes());
+						var currentTimeNoSec = h + ":" + m;
+						
+						//store the strings based on whether the id already exists on the page or not
+						var str = "";
+						
+						//if the lab is closed then you don't need to check anything else - just print the open time
+						if(currentTimeNoSec <= openTime || currentTimeNoSec >= closeTime){
+							//the lab is closed so you should show the closed response
+							str = "<table id="+roomNumber+" style='background-color: #9e0b0f;' width='100%'><tr><td><b>"+roomName + "</b></td> <td>Lab opens at "+convertTimeTo12(openTime)+"</td></tr></table>";
+							idExistCheck(roomNumber, str);
+						}
+						else{
+							//the lab is open so determine what should be displayed
+							if(json.data.length == 0){
+								//there are no classes for this room today
+								str = "<table id="+roomNumber+" style='background-color: #197b30;' width='100%'><tr><td><b>"+roomName + "</b></td> <td>No Upcoming Classes - Closes at "+convertTimeTo12(closeTime)+"</td></tr></table>";								
 								idExistCheck(roomNumber, str);
-							}
-							else{
-								//the lab is open so determine what should be displayed
-								if(json.data.length == 0){
-									//there are no classes for this room today
-									str = "<table id="+roomNumber+" style='background-color: #197b30;' width='100%'><tr><td><b>"+roomName + "</b></td> <td>No Upcoming Classes - Closes at "+convertTimeTo12(closeTime)+"</td></tr></table>";								
+							}//end of if
+							else if(json.data.length == 1){
+								//there is 1 class scheduled for thie room today
+								//the current time
+								var currentTimeS = new Date();
+								var currentHours = addZero(currentTimeS.getHours());
+								var currentMinutes = addZero(currentTimeS.getMinutes());
+								var currentTime =  currentHours+":"+currentMinutes;
+								//class start time and end time
+								var startTime = json.data[0].start.substring(0,5);
+								var endTime = json.data[0].end.substring(0,5);
+								
+								if(currentTime < startTime){
+									str = "<table id="+roomNumber+" style='background-color: #1458a9;'width='100%'><tr><td><b>"+roomName + "</b></td><td><b>"+ json.data[0].meeting + "</b> " + convertTimeTo12(startTime) + "-" + convertTimeTo12(endTime) +"</td></tr></table>";							
 									idExistCheck(roomNumber, str);
 								}//end of if
-								else if(json.data.length == 1){
-									//there is 1 class scheduled for thie room today
-									//the current time
+								else if(startTime <= currentTime && currentTime <= endTime){
+									str = "<table id="+roomNumber+" style='background-color: #9e0b0f;'width='100%'><tr><td><b>"+roomName + "</b></td><td>Class in Session</td></tr></table>";								
+									idExistCheck(roomNumber, str);
+								}//end of else if
+								else if(currentTime > endTime){
+									//This class is past and is not important to the schedule
+									str = "<table id="+roomNumber+" style='background-color: #197b30;' width='100%'><tr><td><b>"+roomName + "</b></td> <td>No Upcoming Classes - Closes at "+convertTimeTo12(closeTime)+"</td></tr></table>";									
+									idExistCheck(roomNumber, str);
+								}//end of else if
+							}//end of else if
+							else if(json.data.length > 1){
+								//there are more then 1 class scheduled for this room today
+								//importantClass keeps track of the i value for the class that should be displayed
+								var importantClass = -1;
+								//howFar is the difference between the start time of a class and the current time in seconds
+								var howFar = -1;
+								//howFar is the difference between the start time of a class and the current time in seconds - BUT of the previous class or the most recent important class
+								var prevHowFar = -1;
+								//go through each class that is scheduled on this day for this room
+								for(var i= 0, len = json.data.length; i < len; i++){
 									var currentTimeS = new Date();
 									var currentHours = addZero(currentTimeS.getHours());
 									var currentMinutes = addZero(currentTimeS.getMinutes());
 									var currentTime =  currentHours+":"+currentMinutes;
-									//class start time and end time
-									var startTime = json.data[0].start.substring(0,5);
-									var endTime = json.data[0].end.substring(0,5);
+									//the start time and end time of this class without the seconds
+									var startTime = json.data[i].start.substring(0,5);
+									var endTime = json.data[i].end.substring(0,5);
 									
-									if(currentTime < startTime){
-										str = "<table id="+roomNumber+" style='background-color: #1458a9;'width='100%'><tr><td><b>"+roomName + "</b></td><td><b>"+ json.data[0].meeting + "</b> " + convertTimeTo12(startTime) + "-" + convertTimeTo12(endTime) +"</td></tr></table>";							
+									if(startTime <= currentTime && currentTime <= endTime){
+										//class is in session
+										str = "<table id="+roomNumber+" style='background-color: #9e0b0f;'width='100%'><tr><td><b>"+roomName + "</b></td><td>Class in Session</td></tr></table>";									
 										idExistCheck(roomNumber, str);
-									}//end of if
-									else if(startTime <= currentTime && currentTime <= endTime){
-										str = "<table id="+roomNumber+" style='background-color: #9e0b0f;'width='100%'><tr><td><b>"+roomName + "</b></td><td>Class in Session</td></tr></table>";								
-										idExistCheck(roomNumber, str);
-									}//end of else if
-									else if(currentTime > endTime){
-										//This class is past and is not important to the schedule
-										str = "<table id="+roomNumber+" style='background-color: #197b30;' width='100%'><tr><td><b>"+roomName + "</b></td> <td>No Upcoming Classes - Closes at "+convertTimeTo12(closeTime)+"</td></tr></table>";									
-										idExistCheck(roomNumber, str);
-									}//end of else if
-								}//end of else if
-								else if(json.data.length > 1){
-									//there are more then 1 class scheduled for this room today
-									//importantClass keeps track of the i value for the class that should be displayed
-									var importantClass = -1;
-									//howFar is the difference between the start time of a class and the current time in seconds
-									var howFar = -1;
-									//howFar is the difference between the start time of a class and the current time in seconds - BUT of the previous class or the most recent important class
-									var prevHowFar = -1;
-									//go through each class that is scheduled on this day for this room
-									for(var i= 0, len = json.data.length; i < len; i++){
-										var currentTimeS = new Date();
-										var currentHours = addZero(currentTimeS.getHours());
-										var currentMinutes = addZero(currentTimeS.getMinutes());
-										var currentTime =  currentHours+":"+currentMinutes;
-										//the start time and end time of this class without the seconds
-										var startTime = json.data[i].start.substring(0,5);
-										var endTime = json.data[i].end.substring(0,5);
 										
-										if(startTime <= currentTime && currentTime <= endTime){
-											//class is in session
-											str = "<table id="+roomNumber+" style='background-color: #9e0b0f;'width='100%'><tr><td><b>"+roomName + "</b></td><td>Class in Session</td></tr></table>";									
-											idExistCheck(roomNumber, str);
-											
-											//This will end the for loop because there is a class in session and we no longer care to know about the rest of the classes
-											i = len;
+										//This will end the for loop because there is a class in session and we no longer care to know about the rest of the classes
+										i = len;
+									}
+									else if(startTime > currentTime){
+										//this class might be the upcoming class and we need to compare to the other classes to see which is up first
+										//this is howFar away this class is from right now
+										howFar = toSeconds(startTime) - toSeconds(currentTime);
+										//if this is the first time through the prevHowFar will not yet have been set
+										if(prevHowFar == -1){
+											prevHowFar = howFar;
+											//also set importantClass to this class
+											importantClass = i;
 										}
-										else if(startTime > currentTime){
-											//this class might be the upcoming class and we need to compare to the other classes to see which is up first
-											//this is howFar away this class is from right now
-											howFar = toSeconds(startTime) - toSeconds(currentTime);
-											//if this is the first time through the prevHowFar will not yet have been set
-											if(prevHowFar == -1){
-												prevHowFar = howFar;
-												//also set importantClass to this class
-												importantClass = i;
-											}
-											//if the difference between this class and now is smaller then the previous class and now then set the new important class to this i
-											else if(howFar < prevHowFar){
-												importantClass = i;
-												prevHowFar = howFar;
-											}
-											if(i+1 == len){
-												//if this is the last time around the for loop then print out
+										//if the difference between this class and now is smaller then the previous class and now then set the new important class to this i
+										else if(howFar < prevHowFar){
+											importantClass = i;
+											prevHowFar = howFar;
+										}
+										if(i+1 == len){
+											//if this is the last time around the for loop then print out
+											str = "<table id="+roomNumber+" style='background-color: #1458a9;'width='100%'><tr><td><b>"+roomName + "</b></td><td><b>"+ json.data[importantClass].meeting + "</b> " + convertTimeTo12(json.data[importantClass].start.substring(0,5)) + "-" + convertTimeTo12(json.data[importantClass].end.substring(0,5)) +"</td></tr></table>";									
+											idExistCheck(roomNumber, str);
+										}
+									}//end of else if
+									else{
+										//this class time is already past
+										if(i+1 == len){
+											//if this is the last time around the for loop then print out
+											if(importantClass > -1){
+												//if the important class has been set then print the imfomation about that class
 												str = "<table id="+roomNumber+" style='background-color: #1458a9;'width='100%'><tr><td><b>"+roomName + "</b></td><td><b>"+ json.data[importantClass].meeting + "</b> " + convertTimeTo12(json.data[importantClass].start.substring(0,5)) + "-" + convertTimeTo12(json.data[importantClass].end.substring(0,5)) +"</td></tr></table>";									
 												idExistCheck(roomNumber, str);
 											}
-										}//end of else if
-										else{
-											//this class time is already past
-											if(i+1 == len){
-												//if this is the last time around the for loop then print out
-												if(importantClass > -1){
-													//if the important class has been set then print the imfomation about that class
-													str = "<table id="+roomNumber+" style='background-color: #1458a9;'width='100%'><tr><td><b>"+roomName + "</b></td><td><b>"+ json.data[importantClass].meeting + "</b> " + convertTimeTo12(json.data[importantClass].start.substring(0,5)) + "-" + convertTimeTo12(json.data[importantClass].end.substring(0,5)) +"</td></tr></table>";									
-													idExistCheck(roomNumber, str);
-												}
-												else{
-													//if the important class has not been set then there are no upcoming classes
-													str = "<table id="+roomNumber+" style='background-color: #197b30;' width='100%'><tr><td><b>"+roomName + "</b></td> <td>No Upcoming Classes - Closes at "+convertTimeTo12(closeTime)+"</td></tr></table>";								
-													idExistCheck(roomNumber, str);
-												}//end of else
-											}//end of if
-										}//end of else
-									}//end of for
-								}//end of else if
-							}//end of else
-						}//end of success
-					);//end of getJSON
-				},30000000);//end of setTimeout
+											else{
+												//if the important class has not been set then there are no upcoming classes
+												str = "<table id="+roomNumber+" style='background-color: #197b30;' width='100%'><tr><td><b>"+roomName + "</b></td> <td>No Upcoming Classes - Closes at "+convertTimeTo12(closeTime)+"</td></tr></table>";								
+												idExistCheck(roomNumber, str);
+											}//end of else
+										}//end of if
+									}//end of else
+								}//end of for
+							}//end of else if
+						}//end of else
+					}//end of success
+				);//end of getJSON
 			}//end of function createMiniSchedule
 			
 			/*
